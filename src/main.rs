2f06c7d9c8eb5083c36ac5de6a59aee9480f1936
Chen Haowei <haoweichenchw@gmail.com>
2022-09-03 22:38:02 +0800

    added some logic, not done

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,22 @@ use rand::Rng;
 use std::io;
 
 fn main() {
-    let x:u8 = rand::thread_rng().gen_range(1..101);
+    let num:u8 = rand::thread_rng().gen_range(1..101);
     println!("Take a guess(1-100):");
     let mut input = String::new();
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Please input a number!");
-    
-    println!("Your guess was {}, which is {} than the number",)
-    
+    loop{
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Please input a number!");
+
+        if input.parse() > num {
+            println!("Your guess was {}, which is greater than the number", num);
+        }else if input.parse() < num {
+            println!("Your guess was {}, which is lesser than the number", num);
+        }else {
+            println!("Your guess is correct! The number is {}", num);
+            break;
+        }
+    }
 }
