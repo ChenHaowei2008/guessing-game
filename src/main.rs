4b25a72a8ff58a7cf2d73c2d4377a8cc0427a229
Chen Haowei <haoweichenchw@gmail.com>
2022-09-04 08:50:46 +0800

    Figured it out :)

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,25 @@ use std::io;
 
 fn main() {
     let num:u8 = rand::thread_rng().gen_range(1..101);
-    println!("Take a guess(1-100):");
-    let mut input = String::new();
 
     loop{
+        println!("Take a guess(1-100):");
+        let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("Please input a number!");
 
-        if input.parse() > num {
-            println!("Your guess was {}, which is greater than the number", num);
-        }else if input.parse() < num {
-            println!("Your guess was {}, which is lesser than the number", num);
+        let input: u8 = match input.trim().parse(){
+            Ok(n) => n,
+            Err(_) => continue,
+        };
+
+        if input > num{
+            println!("Your guess was {}, which is greater than the number", input);
+        }else if input < num {
+            println!("Your guess was {}, which is lesser than the number", input);
         }else {
-            println!("Your guess is correct! The number is {}", num);
+            println!("Your guess is correct! The number is {}", input);
             break;
         }
     }
