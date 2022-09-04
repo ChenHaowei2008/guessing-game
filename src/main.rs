729f017eb9f066c25b666e9021ed9d2da694c5c0
Chen Haowei <haoweichenchw@gmail.com>
2022-09-04 09:02:19 +0800

    made replayable and cleaned up some code

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,45 @@ use rand::Rng;
 use std::io;
 
 fn main() {
-    let num:u8 = rand::thread_rng().gen_range(1..101);
 
     loop{
-        println!("Take a guess(1-100):");
         let mut input = String::new();
+        let num:u8 = rand::thread_rng().gen_range(1..101);
+
+        println!("Welcome to guessing game!");
+
+        loop{
+            let mut guess = String::new();
+
+            println!("Take a guess(1-100):");
+    
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("An error occurred while reading the line!");
+    
+            let guess: u8 = match guess.trim().parse(){
+                Ok(n) => n,
+                Err(_) => continue,
+            };
+    
+            if guess > num {
+                println!("Your guess was {guess}, which is greater than the number");
+            }else if guess < num {
+                println!("Your guess was {guess}, which is lesser than the number");
+            }else {
+                println!("Your guess is correct! The number is {guess}");
+                break;
+            }
+        }
+        println!("Do you want to play again?[Y/n]");
+
         io::stdin()
             .read_line(&mut input)
-            .expect("Please input a number!");
-
-        let input: u8 = match input.trim().parse(){
-            Ok(n) => n,
-            Err(_) => continue,
-        };
-
-        if input > num{
-            println!("Your guess was {}, which is greater than the number", input);
-        }else if input < num {
-            println!("Your guess was {}, which is lesser than the number", input);
-        }else {
-            println!("Your guess is correct! The number is {}", input);
-            break;
+            .expect("An error occurred while reading the line!");
+        
+        if input.trim().to_ascii_lowercase() == "n" {
+            println!("Thanks for playing!");
+            break
         }
     }
 }
